5b52bc96507b1128abe001db9d4df278770a748f
Anton Pavlyukevich <69690074+itonyluke@users.noreply.github.com>
2022-01-05 14:48:37 +0300

    Update life_expectancy.cpp

--- life_expectancy.cpp
+++ life_expectancy.cpp
@@ -19,8 +19,6 @@
 
 // built and tested on Darwin Kernel Version 18.7.0: Tue Jun 22 19:37:08 PDT 2021; root:xnu-4903.278.70~1/RELEASE_X86_64 x86_64
 
-
-
 #include <iostream>
 
 #define B_RED "\033[91m"
